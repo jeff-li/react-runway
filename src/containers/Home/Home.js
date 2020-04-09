d188af7a9bbe33c0709cdba3d72e2ca847f99517
Jeff Li <jeff.li@autocase.com>
2020-04-08 23:04:44 -0400

    Home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import Layout from 'antd/lib/layout';
+import HomeMenu from '../../components/HomeMenu';
 
-const Home = () => (
-  <div>Home Page</div>
-);
+import './home.scss';
+
+const { Header, Sider, Content, Footer } = Layout;
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: false,
+    };
+  }
+
+  onCollapse = (collapsed) => {
+    this.setState({ collapsed });
+  };
+
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <HomeMenu />
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
+}
 
 export default Home;
+{/* <Sidebar collapsed={this.state.collapsed} /> */}
